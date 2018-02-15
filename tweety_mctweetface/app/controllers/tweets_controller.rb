bfa02ef0292f9543b857fddf5a8c0f81988a9bbd
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-02-14 23:18:59 -0500

    add code from day2 code-along

--- tweety_mctweetface/app/controllers/tweets_controller.rb
+++ tweety_mctweetface/app/controllers/tweets_controller.rb
@@ -1,6 +1,8 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+
+  include TweetsHelper
   # GET /tweets
   # GET /tweets.json
   def index
@@ -24,10 +26,12 @@ class TweetsController < ApplicationController
   # POST /tweets
   # POST /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = Tweet.create(tweet_params)
+    @tweet = get_tagged(@tweet)
 
     respond_to do |format|
       if @tweet.save
+
         format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
@@ -69,6 +73,6 @@ class TweetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def tweet_params
-      params.require(:tweet).permit(:message, :user_id)
+      params.require(:tweet).permit(:message, :user_id, :link)
     end
 end
--- tweety_mctweetface/config/routes.rb
+++ tweety_mctweetface/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'unfollow' => 'epicenter#unfollow'
 
+  get 'tag_tweets' => 'epicenter#tag_tweets'
   resources :tweets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
